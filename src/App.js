90b50f1b21e0b48116b18d5a9b6950525ac42310
mrashishraj <mr.ashishrajyadav@gmail.com>
2022-12-03 14:51:10 +0530

    setup react app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+const stylingText = {
+  backgroundColor:"green",
+  borderRadius:'10px',
+}
+
+function App() {
+  return (
+    <div className="App" style={stylingText}>
+      Ashish Raj Hello world
+      <h1></h1>
+    </div>
+  );
+}
+
+export default App;
